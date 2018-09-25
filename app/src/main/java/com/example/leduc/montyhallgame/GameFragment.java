af352c4a421283cbfbee4b88941e2d2dc53b336e
Khang <35643228+SLUkdle15@users.noreply.github.com>
2018-09-24 22:22:44 -0400

    change
    finish the first stage of the game:
    Hint door opens to show goat

---
+++ app/src/main/java/com/example/leduc/montyhallgame/GameFragment.java
@@ -0,0 +1,180 @@
+package com.example.leduc.montyhallgame;
+
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStructure;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GameFragment extends Fragment{
+    private boolean[] dict;
+    private boolean[] door_stage;
+    private static TextView tv;
+    private Timer t;
+    private int chosen_index = -1;
+    private int hint_door = -1;
+    private int the_other_door = -1;
+    private int car_index;
+
+    private void calculated_hint_door(int chosen_index){
+        if(chosen_index == 0){
+            get_hint_door(1, 2);
+        }else if(chosen_index == 2){
+            get_hint_door(0, 1);
+        }else{
+            get_hint_door(0,2);
+        }
+    }
+
+    private void get_hint_door(int a, int b){
+        double c = Math.random();
+        if(c > 0.5){
+            hint_door = a;
+            the_other_door = b;
+        }else{
+            hint_door = b;
+            the_other_door = a;
+        }
+    }
+
+
+    public GameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_game, container, false);
+
+        View door1 = root.findViewById(R.id.door1);
+        View door2 = root.findViewById(R.id.door2);
+        View door3 = root.findViewById(R.id.door3);
+
+        tv = root.findViewById(R.id.prompt);
+
+        final ArrayList<View> list_View = new ArrayList<>();
+        dict = new boolean[]{false, false, false};
+        door_stage = new boolean[]{false, false, false};
+        list_View.add(door1);
+        list_View.add(door2);
+        list_View.add(door3);
+
+        //randomly assign which one has a car.
+        car_index = (int) (Math.random() * 2);
+        dict[car_index] = true;
+
+
+        //TODO load the stage here after reconstructing
+
+        //listener for door
+        for(int i = 0; i < list_View.size(); i ++){
+            final int finalI = i;
+            list_View.get(finalI).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //run through all the close door
+                    chosen_index = finalI;
+                    for (int j = 0; j < dict.length; j++){
+                        ImageButton not_chose_door = (ImageButton) list_View.get(j);
+                        // if the door is chosen, display the image
+                        // if the user change the chosen door, reset the chosen door image
+                        if(j!= chosen_index && !door_stage[j]){
+                            not_chose_door.setImageLevel(0);
+                        }else if(j == chosen_index){
+                            not_chose_door.setImageLevel(1);
+                        }
+                    }
+                }
+            });
+        }
+
+        //Button listener
+        //TODO disable image clickable while countdown
+        Button bt = root.findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // stage 1: all door were closed
+                if(!door_stage[0] && !door_stage[1] && !door_stage[2] && chosen_index != -1){
+                    //find the hint door.
+                    for(int i = 0; i < list_View.size(); i++){
+                        //if chosen is a goat
+                        if (!dict[chosen_index]){
+                            if(!dict[i] && i != chosen_index){
+                                hint_door = i;
+                                the_other_door = car_index;
+                            }
+                        }else{
+                            //pull a 50 - 50 to open the hind door if chosen door is a car
+                            calculated_hint_door(chosen_index);
+                        }
+                    }
+
+                    //start the timer
+                    t = new Timer();
+                    t.schedule(new TimerTask() {
+                        int count = 3;
+
+                        @Override
+                        public void run() {
+                            getActivity().runOnUiThread(new Runnable() {
+                                ImageButton Hint_door = (ImageButton) list_View.get(hint_door);
+                                ImageButton The_Other_door = (ImageButton) list_View.get(the_other_door);
+                                @Override
+                                public void run() {
+                                    tv.setText(R.string.nortify_hint_door);
+                                    if(count <= 6 && count >= 4){
+                                        Hint_door.setImageLevel(count);
+                                        The_Other_door.setImageLevel(count);
+                                    }else if (count > 6){
+                                        Hint_door.setImageLevel(3);
+                                        Hint_door.setClickable(false);
+                                        door_stage[hint_door] = true;
+                                        The_Other_door.setImageLevel(0);
+                                        tv.setText(R.string.ask);
+                                        t.cancel();
+                                    }
+                                    count++;
+                                }
+                            });
+
+                        }
+                    }, 0, 1000);
+                }
+                //stage 2 if the hint door is opened
+            }
+        });
+
+
+        return root;
+    }
+
+}
---
+++ app/src/main/java/com/example/leduc/montyhallgame/MainFragment.java
@@ -0,0 +1,113 @@
+package com.example.leduc.montyhallgame;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public static final String PREF_NAME = "MontyHall";
+    public static final String NEW_CLICKED = "NewClicked";
+
+    private OnFragmentInteractionListener mListener;
+    private int x = 0;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+    //initialize data
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        View aboutButton = rootView.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get activity from the fragment it in
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.about_title_text);
+                builder.setMessage(R.string.about);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        View newButton = rootView.findViewById(R.id.new_button);
+        newButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor pref_ed = getActivity().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+                        .edit();
+                pref_ed.putBoolean(NEW_CLICKED, true).apply();
+                Intent intent = new Intent(getActivity(), GameActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
